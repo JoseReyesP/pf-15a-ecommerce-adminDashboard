c49dc27939a6fe0fedf8dfbfaa55d01e78fe38bd
Luis Reyes <relujo@hotmail.com>
2023-12-12 16:57:08 -0500

    Update NewProduct.js

--- admin-dashboard/src/core/NewProduct.js
+++ admin-dashboard/src/core/NewProduct.js
@@ -59,7 +59,7 @@ const NewProduct = () => {
         "https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.jpg"
       );
 
-      await axios.post("http://pf-15a.up.railway.app/api/product", formData, {
+      await axios.post("https://pf-15a.up.railway.app/api/product", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
